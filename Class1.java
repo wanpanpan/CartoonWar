4ebb493bb5c411f2eb63d17a14d4e18a64cfc549
wanpanpan <53605440+wanpanpan@users.noreply.github.com>
2019-08-16 18:33:06 +0800

    学生系统

---
+++ Class1.java
@@ -0,0 +1,27 @@
+package Student;
+
+/**
+ * @author neuedu_WP1
+ *
+ */
+public class Class1 {
+	public int id;                // 学生ID
+	public String name;           // 学生姓名
+	public char sex;              // 学生性别
+	public int age;               // 学生年龄
+	public String class0;         // 学生班级
+	public String address;        // 学生地址
+	public String phoneNum;       // 联系方式
+	public String email;          // 电子邮箱
+	public String password;       // 密码
+	@Override
+	public String toString() {
+		return "Class1 [id=" + id + ", name=" + name + ", sex=" + sex + ", age=" + age + ", class0=" + class0
+				+ ", address=" + address + ", phoneNum=" + phoneNum + ", email=" + email + ", password=" + password
+				+ "]";
+	}
+	
+}
+
+
+
